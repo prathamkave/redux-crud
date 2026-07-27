be5f0c85f5f283409e684f1f3b212e177d243402
Pratham Kave <kavepratham@gmail.com>
2026-07-27 11:19:23 +0530

    Updated Logo & Name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,11 @@ const Header = () => {
 						className='group flex items-center gap-3'
 					>
 						<div className='flex h-9 w-9 items-center justify-center rounded-xl border border-white/10 bg-white/5 text-sm font-bold text-white transition group-hover:bg-white/10'>
-							N
+							<img
+								src='/image.png'
+								alt='N'
+								className='rounded-xl'
+							/>
 						</div>
 
 						<div className='leading-none'>
